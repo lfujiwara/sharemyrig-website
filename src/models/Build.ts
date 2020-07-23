116017aa36918efec5a0dada4cacf5d5aee00c12
Lucas Toshio Löschner Fujiwara <40678033+lfujiwara@users.noreply.github.com>
2020-07-22 23:21:59 -0300

    Update models Include classes to use as reference/helpers

--- src/models/Build.ts
+++ src/models/Build.ts
@@ -1,4 +1,4 @@
-import { IComponent } from "./Component";
+import { IComponent, Component } from "./Component";
 
 export interface IBuild {
   cpu?: IComponent;
@@ -20,20 +20,59 @@ export interface IBuild {
 }
 
 export class Build implements IBuild {
-  cpu?: IComponent;
-  cpuCooler?: IComponent;
-  gpus: [IComponent];
+  cpu: IComponent;
+  cpuCooler: IComponent;
+  gpus: IComponent[];
   motherboard: IComponent;
-  memory: [IComponent];
-  storageDrives: [IComponent];
-  case?: IComponent;
+  memory: IComponent[];
+  storageDrives: IComponent[];
+  case: IComponent;
   psu: IComponent;
-  accessories: [IComponent];
-  monitors: [IComponent];
-  headphone?: IComponent;
-  microphone?: IComponent;
-  keyboard?: IComponent;
-  soundCard?: IComponent;
-  mouse?: IComponent;
-  mousePad?: IComponent;
+  accessories: IComponent[];
+  monitors: IComponent[];
+  headphone: IComponent;
+  microphone: IComponent;
+  keyboard: IComponent;
+  soundCard: IComponent;
+  mouse: IComponent;
+  mousePad: IComponent;
+
+  constructor() {
+    this.cpu = new Component();
+    this.cpuCooler = new Component();
+    this.motherboard = new Component();
+    this.case = new Component();
+    this.psu = new Component();
+    this.headphone = new Component();
+    this.microphone = new Component();
+    this.keyboard = new Component();
+    this.soundCard = new Component();
+    this.mouse = new Component();
+    this.mousePad = new Component();
+    this.gpus = [];
+    this.memory = [];
+    this.storageDrives = [];
+    this.accessories = [];
+    this.monitors = [];
+  }
+
+  total() {
+    return (
+      (this.cpu.price || 0) +
+      (this.cpuCooler.price || 0) +
+      (this.gpus.reduce((a, c) => a + (c.price || 0), 0) || 0) +
+      (this.motherboard.price || 0) +
+      (this.memory.reduce((a, c) => a + (c.price || 0), 0) || 0) +
+      (this.storageDrives.reduce((a, c) => a + (c.price || 0), 0) || 0) +
+      (this.case.price || 0) +
+      (this.psu.price || 0) +
+      (this.accessories.reduce((a, c) => a + (c.price || 0), 0) || 0) +
+      (this.monitors.reduce((a, c) => a + (c.price || 0), 0) || 0) +
+      (this.headphone.price || 0) +
+      (this.keyboard.price || 0) +
+      (this.soundCard.price || 0) +
+      (this.mouse.price || 0) +
+      (this.mousePad.price || 0)
+    );
+  }
 }
